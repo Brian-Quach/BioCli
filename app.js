657ae88f5078a3de905266dd063edb31ff1495a4
brian-quach <briiquach@gmail.com>
2018-10-12 16:58:43 -0400

    Helloworld.express

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+//app.use(express.static('static'));
+
+//var bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
+app.use(function (req, res, next){
+    console.log("HTTP request", req.method, req.url, req.body);
+    next();
+});
+
+app.use(function (req, res, next){
+    console.log("HTTP Response", res.statusCode);
+});
+
+const PORT = 3000;
+app.listen(PORT);
