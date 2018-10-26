6b71f6965973df9663e92eabb8ce307b33fb5557
Brian.quach <briquach@gmail.com>
2018-10-26 16:06:10 -0400

    Add user input

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,8 +1,32 @@
-function processCmd(){
-    let keyCode = event.keyCode;
-    if (keyCode === 13){
-        console.log("Enter!!!");
-    }
-}
-
 cmdIn = document.getElementById('cmd_in');
+
+cmdIn.addEventListener("blur", function() {
+    cmdIn.focus();
+}, true);
+
+cmdIn.addEventListener('keydown', function (e) {
+    let key = e.which || e.keyCode;
+    if (key === 13) { // Enter
+        console.log(cmdIn.value);
+        cmdIn.value = "";
+    } else if (key === 38) { // Up
+        console.log("Up");
+    } else if (key === 40) { // Down
+        console.log("Down");
+    } else if (key === 9) {  // Tab
+        e.preventDefault();
+        console.log("Tab");
+    } else if (key === 18) { // Ctrl - Not working (TODO)
+        e.preventDefault();
+        console.log("Ctrl");
+    }
+});
+
+function cHandler(e) {
+    e.preventDefault();
+    let key = e.which || e.keyCode;
+    if (key === 67) { // c
+        console.log("Ctrl + C");
+    }
+
+}
