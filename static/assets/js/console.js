10d020747b2f488ab139d6bdcf951628e85fc454
brian-quach <briiquach@gmail.com>
2018-10-29 00:19:01 -0400

    TypeLine() func

---
+++ static/assets/js/console.js
@@ -0,0 +1,84 @@
+let ctrlDown = false;
+let outputCount = 0;
+
+cmdIn = document.getElementById('cmd_in');
+cmdOut = document.getElementById('output');
+
+
+cmdIn.addEventListener("blur", function() {
+    cmdIn.focus();
+}, true);
+
+
+cmdIn.addEventListener('keydown', function (e) {
+    let key = e.which || e.keyCode;
+    if (key === 13) { // Enter
+        printLine(cmdIn.value);
+        cmdIn.value = "";
+    } else if (key === 38) { // Up
+        console.log("Up");
+    } else if (key === 40) { // Down
+        console.log("Down");
+        printLine("test");
+    } else if (key === 9) {  // Tab
+        e.preventDefault();
+        console.log("Tab");
+    } else if (key === 17) { // Ctrl
+        e.preventDefault();
+        ctrlDown = true;
+    } else if (key === 67) { // C
+        if (ctrlDown){
+            console.log("Ctrl + C");
+        }
+    }
+});
+
+
+cmdIn.addEventListener('keyup', function (e) {
+    let key = e.which || e.keyCode;
+    if (key === 17) {
+        e.preventDefault();
+        ctrlDown = false;
+    }
+});
+
+function createTextSpan(type, text){
+    let newElement = document.createElement("span");
+    newElement.classList.add(type);
+    newElement.id = "out-"+outputCount.toString();
+    outputCount++;
+    newElement.textContent = text;
+
+    return newElement;
+}
+
+
+function printLine(text){
+    let newLine = document.createElement("div");
+    let lineText = createTextSpan("output", text);
+    newLine.appendChild(lineText);
+
+    cmdOut.appendChild(newLine);
+}
+
+function typeLine(text){
+    let newLine = document.createElement("div");
+    cmdOut.appendChild(newLine);
+    let newSpan = document.createElement("span");
+    newSpan.classList.add("output");
+    newSpan.id = "out-"+outputCount.toString();
+    outputCount++;
+    newLine.appendChild(newSpan);
+
+    new Typed("#"+newSpan.id, {
+        strings: [text],
+        typeSpeed: 30,
+        loop: false,
+        cursorChar: '',
+        onComplete: function(self) {
+            printLine(cmdIn.value);
+            cmdIn.value = "";
+        },
+    });
+
+}
